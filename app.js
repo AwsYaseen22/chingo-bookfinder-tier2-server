78b33bd049709835a49b5dfcdbb3d3aeefab5b0c
AwsYaseen22 <aws.yaseen.22@gmail.com>
2023-02-25 01:01:59 +0300

    connect the server with the front

--- app.js
+++ app.js
@@ -1,10 +1,23 @@
 const dotenv = require( 'dotenv' )
 const express = require( 'express' )
 const cors = require( 'cors' )
+const axios = require( 'axios' )
 
-dotenv.config( { path: "./.env" } );
+require( 'dotenv' ).config()
 const app = express()
 app.use( cors() )
 app.use( express.json() )
+
+const apiKey = process.env.REACT_APP_GOOGLE_API_KEY
+
+app.get( '/api/books/:title', async ( request, response ) => {
+    const title = request.params.title
+    try {
+        const found = await axios.get( `https://www.googleapis.com/books/v1/volumes?q=intitle:${title}&key=${apiKey}` )
+        response.json( found.data )
+    } catch ( error ) {
+        console.error( error );
+    }
+} )
 
 module.exports = app
